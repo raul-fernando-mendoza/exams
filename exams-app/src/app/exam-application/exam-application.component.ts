f992233cddf90a6efe75b8911a73ce1cdb17e485
Raul Mendoza <raul.mendoza@softtek.com>
2020-12-13 16:07:52 -0600

    checkbox working

--- exams-app/src/app/exam-application/exam-application.component.ts
+++ exams-app/src/app/exam-application/exam-application.component.ts
@@ -16,49 +16,95 @@ export class ExamApplicationComponent {
  
   hideRequiredControl = new FormControl(false);
 
-  questionsForm: FormGroup;
+  //questionsForm: FormGroup;
 
-  addressForm = this.fb.group({
+  posicionesForm = this.fb.group({
 
     id: null,
-    questions: this.fb.array([])  
+    posicionesArray: this.fb.array([])  
   });
 
 
-  get questions() : FormArray {
-    return this.addressForm.get("questions") as FormArray
+  get posicionesArray() : FormArray {
+    return this.posicionesForm.get("posicionesArray") as FormArray
   }
 
-  newQuestion(value:boolean, label:string): FormGroup {
-    var f:FormControl = new FormControl();
-    f.setValue(value);
+  newPosicion(
+    id:number, 
+    label:string, 
+    movimiento:number, 
+    pies:number, 
+    shimmi:number, 
+    palomas:number,
+    portDebra:number,
+    craneo:number ): FormGroup {
 
     var group = this.fb.group({
-      id: f,
-      label: new FormControl(false),
+      id: id,
+      label: label,
+      movimiento: new FormControl(movimiento),
+      pies: new FormControl(pies),
+      shimmi: new FormControl(shimmi),
+      palomas: new FormControl(palomas),
+      portDebra: new FormControl(portDebra),
+      craneo: new FormControl(craneo)
     });
     return group;
     
  }
 
- i:boolean = true;
- addQuestion() {
-    this.questions.push(this.newQuestion(this.i, "hole" + this.i));
-    if( this.i == true)
-      { this.i = false}
-    else {
-       this.i = true;
+  db_posiciones = [
+      
+    {
+      id:1,
+      label:"movimiento 1",
+      movimiento: 1,
+      pies: 0,
+      shimmi: 1,
+      palomas: 0,
+      portDebra: 1,
+      craneo: 0     
+    },
+    {
+      id:2,
+      label:"movimiento 2",
+      movimiento: 0,
+      pies: 1,
+      shimmi: 0,
+      palomas: 1,
+      portDebra: 0,
+      craneo: 1  
     }
-  }
+  ];     
 
-  toJson(o){
-    return JSON.stringify(o);
+  initializeFormControls(){
+    
+    var i = 0;
+    for (i=0; i<this.db_posiciones.length; i++) {
+      var p = this.db_posiciones[i]
+      console.debug(p.id + " " + p.label);
+      this.posicionesArray.push(this.newPosicion(
+        p.id,
+        p.label,
+        p.movimiento,
+        p.pies,
+        p.shimmi,
+        p.palomas,
+        p.portDebra,
+        p.craneo          
+        )
+      )
+    }
+  
+
+    //return JSON.stringify(o);
   } 
   exam_id: number;
   constructor(private fb: FormBuilder, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.exam_id = Number(this.route.snapshot.paramMap.get('exam_id'));
+    this.initializeFormControls();
     //alert( "receive:" + this.exam_id)
   }
 
