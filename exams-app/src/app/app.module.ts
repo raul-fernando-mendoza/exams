dd57070f9e902c18156da261a4a278411dd7c229
Raul Fernando Mendoza Huerta <raul_fernando_mendoza@hotmail.com>
2021-07-15 13:19:13 -0500

    adding videojs scripts

--- exams-app/src/app/app.module.ts
+++ exams-app/src/app/app.module.ts
@@ -49,12 +49,8 @@ import { LoginSelectorComponent } from './login-selector/login-selector.componen
 import { WelcomeComponent } from './welcome/welcome.component'; 
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-<<<<<<< HEAD
 import { VideosListComponent } from './videos-list/videos-list.component';
 import { AppVjsPlayerComponent } from './app-vjs-player/app-vjs-player.component'; 
-=======
-import { VideosListComponent } from './videos-list/videos-list.component'; 
->>>>>>> 494fe68 (adding video folders)
 
 
 @NgModule({
