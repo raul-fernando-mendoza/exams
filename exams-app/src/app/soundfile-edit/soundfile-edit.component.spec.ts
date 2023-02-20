23bb48596988fba762edf60cf2d90d944b9af2c6
raul mendoza <raul_fernando_mendoza@hotmail.com>
2023-02-19 22:00:59 -0600

    added module

---
+++ exams-app/src/app/soundfile-edit/soundfile-edit.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SoundfileEditComponent } from './soundfile-edit.component';
+
+describe('SoundfileEditComponent', () => {
+  let component: SoundfileEditComponent;
+  let fixture: ComponentFixture<SoundfileEditComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SoundfileEditComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SoundfileEditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ exams-app/src/app/soundfile-edit/soundfile-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-soundfile-edit',
+  templateUrl: './soundfile-edit.component.html',
+  styleUrls: ['./soundfile-edit.component.css']
+})
+export class SoundfileEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
