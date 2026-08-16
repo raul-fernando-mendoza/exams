cc2fbac6fd3672af51e8733d7ea82becae43a86c
raul-fernando-mendoza <raul_fernando_mendoza@hotmail.com>
2026-08-15 21:48:44 -0600

    using delete only in exam

--- examgradeslist/examgradelist.py
+++ examgradeslist/examgradelist.py
@@ -45,7 +45,6 @@ def gradesListByEvaluatorId(evaluator_id: str):
                         .where('evaluator_uid', '==', evaluator_id)
                         .where("isCompleted", "==", False)
                         .where("isCurrentVersion", "==", True)
-                        .where("isDeleted", "==", False)
         )
 
         exam_grade_data = exam_grade_doc.to_dict() or {}
